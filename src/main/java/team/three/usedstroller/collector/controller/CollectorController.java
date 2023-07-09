7a9a757de826b4a60b909330969accf1ff902b3d
clowncdi <clowncdi85@gmail.com>
2023-07-09 12:14:33 +0900

    feat: 설정 추가 및 swagger ui 추가

---
+++ src/main/java/team/three/usedstroller/collector/controller/CollectorController.java
@@ -0,0 +1,24 @@
+package team.three.usedstroller.collector.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/collector")
+public class CollectorController {
+
+	@GetMapping("/naver-test")
+	@ResponseStatus(HttpStatus.OK)
+	public void test() {
+		log.info("naver test");
+
+	}
+
+}
---
+++ src/main/java/team/three/usedstroller/core/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package team.three.usedstroller.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Swagger address : {ip:port}/swagger-ui/index.html
+ */
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.OAS_30)
+				.useDefaultResponseMessages(false)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("team.three.usedstroller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Used Stroller Swagger")
+				.description("Used Stroller config")
+				.version("1.0")
+				.build();
+	}
+
+
+}
