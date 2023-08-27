6c52d99340a7084dd37403759d641c450d93c6a7
hoonyhoney <hoonyhoeny@gmail.com>
2023-08-27 21:39:52 +0900

    feat: test case추가

--- src/test/java/team/three/usedstroller/UsedStrollerApplicationTests.java
+++ src/test/java/team/three/usedstroller/UsedStrollerApplicationTests.java
@@ -62,4 +62,12 @@ class UsedStrollerApplicationTests {
 
 	}
 
+	@Test
+	void parseIntTest(){
+		String qty = "18,025  개의 상품";
+		String intStr = qty.replaceAll("[^0-9]", "");
+		int i = Integer.parseInt(intStr);
+		System.out.println("i = " + i);
+	}
+
 }
