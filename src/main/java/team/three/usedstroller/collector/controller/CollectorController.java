1c87c51c1b36b9f7f444a7cce8ebffc39c3ef904
hoonyhoney <hoonyhoeny@gmail.com>
2024-03-07 21:59:35 +0900

    fix : 시작페이지 디폴트 값 설정/ DB url 수정

--- src/main/java/team/three/usedstroller/collector/controller/CollectorController.java
+++ src/main/java/team/three/usedstroller/collector/controller/CollectorController.java
@@ -22,64 +22,69 @@ import team.three.usedstroller.collector.service.SecondWearService;
 @Tag(name = "collector", description = "수집기")
 public class CollectorController {
 
-	private final NaverService naverService;
-	private final CarrotService carrotService;
-	private final SecondWearService secondWearService;
-	private final BunJangService bunJangService;
-	private final JunggonaraService junggonaraService;
+  private final NaverService naverService;
+  private final CarrotService carrotService;
+  private final SecondWearService secondWearService;
+  private final BunJangService bunJangService;
+  private final JunggonaraService junggonaraService;
 
-	/**
-	 * 번개장터 '유모차' 검색 결과를 수집한다.
-	 * @runningTime 5초 (약 2800건)
-	 */
-	@PostMapping("/bunjang")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void bunjang() {
-		bunJangService.start();
-	}
+  /**
+   * 번개장터 '유모차' 검색 결과를 수집한다.
+   *
+   * @runningTime 5초 (약 2800건)
+   */
+  @PostMapping("/bunjang")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void bunjang() {
+    bunJangService.start();
+  }
 
-	/**
-	 * 중고나라 '유모차' 검색 결과를 수집한다. (약 18,000건)
-	 * @runningTime 약 20초 (약 10,000건)
-	 */
-	@PostMapping("/junggonara")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void junggonara() {
-		junggonaraService.start();
-	}
+  /**
+   * 중고나라 '유모차' 검색 결과를 수집한다. (약 18,000건)
+   *
+   * @runningTime 약 20초 (약 10,000건)
+   */
+  @PostMapping("/junggonara")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void junggonara() {
+    junggonaraService.start();
+  }
 
   /**
    * 세컨웨어(구 헬로마켓) '유모차' 검색 결과를 수집한다.
+   *
    * @runningTime 약 10초 (약 600건)
    */
   @PostMapping("/secondwear")
   @ResponseStatus(HttpStatus.CREATED)
   public void secondwear() {
-		secondWearService.start();
+    secondWearService.start();
   }
 
-	/**
-	 * 당근마켓 '유모차' 검색 결과를 수집한다.
-	 * "https://www.daangn.com/search/%EC%9C%A0%EB%AA%A8%EC%B0%A8/more/flea_market?page="
-	 * @param startPage 시작 페이지
-	 * @runningTime 500페이지에 약 30분 (약 3000건)
-	 */
-	@PostMapping("/carrot-market")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void carrotMarket(
-			@RequestParam(required = true) Integer startPage,
-			@RequestParam(required = true) Integer endPage) {
-		carrotService.start(startPage, endPage);
-	}
+  /**
+   * 당근마켓 '유모차' 검색 결과를 수집한다.
+   * "https://www.daangn.com/search/%EC%9C%A0%EB%AA%A8%EC%B0%A8/more/flea_market?page="
+   *
+   * @param startPage 시작 페이지
+   * @runningTime 500페이지에 약 30분 (약 3000건)
+   */
+  @PostMapping("/carrot-market")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void carrotMarket(
+      @RequestParam(required = true, defaultValue = "1") Integer startPage,
+      @RequestParam(required = true, defaultValue = "200") Integer endPage) {
+    carrotService.start(startPage, endPage);
+  }
 
-	/**
-	 * 네이버쇼핑 검색 API를 이용해서 '유모차' 검색 결과를 수집한다.
-	 * @runningTime 약 10초 (1000건)
-	 */
-	@PostMapping("/naver-shopping")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void naverShopping() {
-		naverService.start();
-	}
+  /**
+   * 네이버쇼핑 검색 API를 이용해서 '유모차' 검색 결과를 수집한다.
+   *
+   * @runningTime 약 10초 (1000건)
+   */
+  @PostMapping("/naver-shopping")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void naverShopping() {
+    naverService.start();
+  }
 
 }
