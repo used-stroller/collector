0719508bb5ef21a4c3129d5d032760bb9bc3f2b5
hoonyhoney <hoonyhoeny@gmail.com>
2024-03-17 22:17:19 +0900

    fix : 슬랙웹훅 전송방식 변경

--- src/main/java/team/three/usedstroller/collector/util/SlackHook.java
+++ src/main/java/team/three/usedstroller/collector/util/SlackHook.java
@@ -1,19 +1,26 @@
 package team.three.usedstroller.collector.util;
 
-import net.gpedro.integrations.slack.SlackApi;
-import net.gpedro.integrations.slack.SlackMessage;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class SlackHook {
 
   @Value("${slack.url}")
-  private String slackUrl;
+  private String SLACK_WEBHOOK_URL;
 
   public void sendMessage(String channel, int count, double time) {
-    String message = String.format("%s 완료: {%s}건, 수집 시간: {%s}s", channel, count, time);
-    SlackApi api = new SlackApi(slackUrl);
-    api.call(new SlackMessage(message));
+    RestTemplate restTemplate = new RestTemplate();
+    String message = String.format("%s 완료: {%s}건, 수집 시간:{%s}s", channel, count, time);
+    Map<String, Object> request = new HashMap<>();
+    request.put("text", message); //전송할 메세지
+
+    HttpEntity<Map<String, Object>> entity = new HttpEntity<Map<String, Object>>(request);
+    restTemplate.exchange(SLACK_WEBHOOK_URL, HttpMethod.POST, entity, String.class);
   }
 }
